1cf480d117079a2e0081cb03b58c8b2cd7975d3e
KTamas03 <132874272+KTamas03@users.noreply.github.com>
2023-06-18 20:49:40 +1000

    Update Main.py
    update code

--- PyPoll/Main.py
+++ PyPoll/Main.py
@@ -1,64 +1,55 @@
 #PyPoll Analysis to create output to terminal and to text file
 
-#Import OS module to allow to create file paths accross operating systems
-import os
+import os                                                           #import OS module to allow to create file paths accross operating systems
+import csv                                                          #import csv module for reading CSV files
 
-#Module for reading CSV files
-import csv
+csvpath=os.path.join('Resources','election_data.csv')               #source file and location in network folder
 
-#source file and location in network folder
-csvpath=os.path.join('Resources','election_data.csv')
-
-#Name of the output text file and location in network folder
-output_folder = os.path.join('analysis','election_results.txt') 
+output_folder = os.path.join('analysis','election_results.txt')     #name of the output text file and location in network folder 
 
 with open(csvpath) as csvfile, open(output_folder, "w") as output:
-    # csv reader specifies delimiter and variable that holds contents
-    csvreader=csv.reader(csvfile,delimiter=',')
+    csvreader=csv.reader(csvfile,delimiter=',')                     #csv reader specifies delimiter and variable that holds contents
+    csv_header=next(csvreader)                                      #skip header row
 
-    # skip header row
-    csv_header=next(csvreader)
-
-    #Create variable row_count excluding the header row
-    row_count=0
-    vote_counts={}  #create empty library to add count of votes per candidate
+    row_count=0                                                     #create variable row_count excluding the header row
+    vote_counts={}                                                  #create empty library to add count of votes per candidate
     
     for row in csvreader:
-        row_count=row_count+1   #increment row count for each row processed
-        candidate = row[2]      #column C is Candidate Names
+        row_count = row_count + 1                                   #increment row count for each row processed
+        candidate = row[2]                                          #column C in cvs file holds candidate names
     
         if candidate in vote_counts:
-            vote_counts[candidate] = vote_counts[candidate] + 1
+            vote_counts[candidate] = vote_counts[candidate] + 1     #increment vote count for each vote processed by candidate
         else:
-            vote_counts[candidate]=1
+            vote_counts[candidate]= 1
     
-    #Print output to terminal
+    #print output to terminal
     print()
     print("Election Results")
     print()
     print("----------------------------------")
     print()
-    print(f"Total Votes: {row_count}")
+    print(f"Total Votes: {row_count}")                              #print total votes counted in csv file
     print()
     print("----------------------------------")
     print()
 
+    #following 2 variables created to keep track of the candidate with the maximum votes
     max_votes=0
     winner=""
 
-    for candidate, count in vote_counts.items(): # Print the vote counts per candidate
-        percentage=(count/row_count)*100
+    for candidate, count in vote_counts.items():                    #print the vote counts for all the candidates listed in col C of cvs file
+        percentage=(count/row_count)*100                            #percentage of votes by candidate
         print()
-        print(f"{candidate}: {percentage:.3f}%, ({count})")
+        print(f"{candidate}: {percentage:.3f}%, ({count})")         #print candidate name, % of votes won to 3 decimals and count of votes
         print()
 
-
-        if count > max_votes:
-            max_votes = count
+        if count > max_votes:                                       # calculate which candidate has the most votes
+            max_votes = count     
             winner = candidate
     print("----------------------------------")
     print()
-    print(f"Winner: {winner}")
+    print(f"Winner: {winner}")                                      #winner's name is printed based on rows 52 to 54
     print()
     print("----------------------------------")
 
