90fb9e1b86ba15ac910b166902e0965efe435ac9
KTamas03 <132874272+KTamas03@users.noreply.github.com>
2023-06-19 15:01:05 +1000

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,41 +11,39 @@ with open(csvpath) as csvfile, open(output_folder, "w") as output:
     csvreader=csv.reader(csvfile,delimiter=',')                         #csv reader specifies delimiter and variable that holds contents
     csv_header=next(csvreader)                                          #skip header row
 
-    row_count=0                                                         #to count rows excluding the header row
-    total_sum=0                                                         #sum up profit/losses
-    previous_value=0                                                    #previous value in column
+    row_count=1                                                         #to count rows excluding the header row
     change_list=[]                                                      #list to store change in values from one month to the next
     max_increase=0                                                      #to calculate the greatest increase in profits
     max_increase_month=""                                               #to return the month with the greatest increase in profits
     max_decrease=0                                                      #to calculate the greatest decrease in profits  
     max_decrease_month=""                                               #to return the month with the greatest decrease in profits
-
+    first_Row=next(csvreader)                                           #so we compare row 3 to row 2 first
+    prev_net=int(first_Row[1])                                          #setting cell B2 as the previous value to calc change in profit
+    total_sum=prev_net                                                  #start total_sum calc from cell B2
+    
     for row in csvreader:
         row_count = row_count + 1                                       #increment row count for each row processed
-
+        
         if row_count>0:                                                 #exclude header row from sum calculation
             total_sum = total_sum + int(row[1])                         #convert the value in column B to an integer and add it to the total sum
         total_sum_format="${}".format(total_sum)
     
-        change = int(row[1]) - previous_value                           #calculate the change from the previous value
+        change = int(row[1]) - prev_net                                 #calculate the change from the previous value
         change_list.append(change)                                      #append each change calc for each row of data to change_list
-        previous_value = int(row[1])                                    #update the previous value for the next iteration
+        prev_net = int(row[1])                                          #update the previous value for the next iteration
 
         if change > max_increase:                                       #check if the change is the greatest increase
             max_increase = change
             max_increase_format="${}".format(max_increase)
             max_increase_month=row[0]
-        previous_value=int(row[1])
+        prev_net=int(row[1])
 
         if change < max_decrease:                                       #check if the change is the greatest decrease
             max_decrease = change
             max_decrease_format="${}".format(max_decrease)
             max_decrease_month=row[0]
-        previous_value=int(row[1])
+        prev_net=int(row[1])
 
-    #delete first item in change_list using pop
-    change_list.pop(0)                                                  #don't want the first difference as it is row 2 minus the header row, keeping it in distorts the average_change result 
-    
     average_change = sum(change_list) / (row_count-1)                   #calculate the average change
     average_change_format="${:.2f}".format(average_change)
 
